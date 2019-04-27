388024bdc11fa2264a9dabd687e03a1d4245c258
Karl Jones <K4Jo73@gmail.com>
2019-04-27 20:12:30 +0100

    More string tricks and a prompt for input

---
+++ HowTo/PromptForInput.py
@@ -0,0 +1,8 @@
+import random
+import sys
+import os
+
+# This does not work properly in Atom - no prompt appears
+print("What is your name")
+yourname = sys.stdin.readline()
+print("Hello", yourname)
--- HowTo/Strings.py
+++ HowTo/Strings.py
@@ -18,3 +18,24 @@ print("I don't like ", end="")
 print("newlines")
 print('\n' * 5)
 print("but sometimes it is ok")
+
+
+long_string = "i'll catch you if you fall - The Floor"
+print(long_string)
+print(long_string[0:6])
+print(long_string[6])
+print(long_string[-5])
+print(long_string[-9:])
+print(long_string[:-12])
+print(long_string[:4] + ' be there')
+print("%c is my %s letter and my number %d number is %.5f" %
+('X', 'favourite', 1, 0.14))
+print(long_string.capitalize())
+print(long_string.find("Floor"))
+print(long_string.isalpha()) #All Letters
+print(long_string.isalnum()) #All Numbers
+print(len(long_string))
+print(long_string.replace("Floor","Ground"))
+
+quote_list = long_string.split(" ")
+print(quote_list)
