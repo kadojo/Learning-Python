b629a7ceab666d394a00c2bb9f8586dbdcb8363d
DojoDoom <DojoDoom@users.noreply.github.com>
2019-05-21 18:26:16 +0100

    Moved files

---
+++ Dom_Notes/Whiteboard_21-05-19.py
@@ -0,0 +1,3 @@
+import random # Module for random number generators
+import sys # Module that provides access to variables used and maintained by the interpreter
+import os # Module for a portable way of using operating system dependent functionality
